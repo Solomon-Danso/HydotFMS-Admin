11ca834d884484bc9368c8f33625c07c9b9ed498
Solomon <solomondanso2023@gmail.com>
2024-11-06 12:52:20 +0000

    Rent Section Completed

--- src/pages/MainWebConfig/RentACarSlidesDetails.jsx
+++ src/pages/MainWebConfig/RentACarSlidesDetails.jsx
@@ -1,23 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import "../Website/Website.css";
-import Select from 'react-select';
-import styled from 'styled-components';
-import { AdmitButton3, AdmitStudentRole, FormInputStudent, FormLable, FormTextAreaStudent } from '../../data/Profile';
-import { colors } from '../../data/Colors';
-import { categoryGrid, contextMenuItems, continentList, countryList, customers, customersData, customersGrid, emailData, emailGrid, employeeData, employeeGrid, exploreGrid, otherGrid, paymentData, paymentGrid, paymentMethod, paymentReference, productGrid, productImageGrid, products } from '../../data/champion';
-import { GridComponent, ContextMenu, Edit, ExcelExport, Filter, Page, PdfExport, Resize, Sort, ColumnDirective, ColumnsDirective, Inject } from '@syncfusion/ej2-react-grids';
+import { AdmitButton3, AdmitStudentRole, FormInputStudent, FormLable } from '../../data/Profile';
 import { Header } from '../../components';
 import Selector from '../../data/Selector';
 import { Show } from '../../data/Alerts';
 import { apiServer } from '../../data/Endpoint';
 import { AES, enc } from 'crypto-js';
-import { useNavigate } from 'react-router-dom';
-import { Search, Toolbar } from '@syncfusion/ej2-react-grids';
+import { useNavigate, useParams } from 'react-router-dom';
+import { TfiLayoutSlider } from 'react-icons/tfi';
+import { FaCar, FaEdit } from 'react-icons/fa';
+import { MdDelete } from 'react-icons/md';
+import HydotTable from '../../data/HydotTable';
+import {
+  Stepper, Step, StepLabel, Button, Typography, Box
+} from '@mui/material';
+import CheckIcon from '@mui/icons-material/Check';
+import GroupAddIcon from '@mui/icons-material/GroupAdd';
+import VideoLabelIcon from '@mui/icons-material/VideoLabel';
+import { GiBookCover } from "react-icons/gi";
 
 
 
 
-const RentACarSlidesDetails = () => {
+const Explore = () => {
   useEffect(() => {
     const observer = new ResizeObserver(() => {
       try {
@@ -36,26 +41,16 @@ const RentACarSlidesDetails = () => {
     return () => observer.disconnect();
   }, []);
 
+const {rentACarId} = useParams()
 
-const [MenuList, setMenuList] = useState([])
-const [MenuId, setMenuId] = useState("")
-const [CategoryList, setCategoryList] = useState("")
-const [CategoryId, setCategoryId] = useState("")
-const [picture, setPicture] = useState("")
-const [ProductId, setProductId] = useState("")
-const [Title, setTitle] = useState("")
-const [Price, setPrice] = useState("")
-const [Quantity, setQuantity] = useState("")
-const [Size, setSize] = useState("")
-const [Description, setDescription] = useState("")
+
+
 const [previewImage, setPreviewImage] = useState(null); // For image preview
- 
 
+const [Src, setSrc] = useState("")
+const [CoverType, setCoverType] = useState("")
+const [Explore, setExplore] = useState([])
 
-
-
-
-  const [AdminList, setAdminList] = useState([])
 
 
 
@@ -82,32 +77,15 @@ useEffect(() => {
 }, []);
 
 
-useEffect(()=>{
-
-  const formData = new FormData();
-  formData.append("AdminId",userInfo.UserId)
-
-fetch(apiServer+"ViewProductAdmin",{
-  method: "POST",
-      headers: {
-        'UserId': userInfo.UserId,         
-        'SessionId': userInfo.SessionId    
-      },
-      body:formData
-})
-.then(res=>res.json())
-.then(data=>setAdminList(data))
-.catch(err=>console.error(err))
 
 
-},[userInfo])
 
 useEffect(()=>{
 
     const formData = new FormData();
     formData.append("AdminId",userInfo.UserId)
   
-  fetch(apiServer+"ViewMenu",{
+  fetch(apiServer+"ViewAllRentACarSRC",{
     method: "POST",
         headers: {
           'UserId': userInfo.UserId,         
@@ -116,33 +94,16 @@ useEffect(()=>{
         body:formData
   })
   .then(res=>res.json())
-  .then(data=>setMenuList(data))
-  .catch(err=>console.error(err))
-  
-  
-  },[userInfo])
-
-
-
-  useEffect(()=>{
-
-    const formData = new FormData();
-    formData.append("AdminId",userInfo.UserId)
-  
-  fetch(apiServer+"ViewCategory",{
-    method: "POST",
-        headers: {
-          'UserId': userInfo.UserId,         
-          'SessionId': userInfo.SessionId    
-        },
-        body:formData
+  .then(data=>{
+    setExplore(data)
   })
-  .then(res=>res.json())
-  .then(data=>setCategoryList(data))
   .catch(err=>console.error(err))
   
   
   },[userInfo])
+
+
+
 
 
 
@@ -157,20 +118,14 @@ Show.showLoading("Processing Data");
 const formData = new FormData()
 
 formData.append("AdminId",userInfo.UserId)
-formData.append("MenuId",MenuId);
-formData.append("CategoryId",CategoryId);
-formData.append("Picture",picture);
-formData.append("Title",Title);
-formData.append("Price",Price);
-formData.append("Quantity",Quantity);
-formData.append("Size",Size);
-formData.append("ProductId",ProductId);
-formData.append("Description",Description);
+formData.append("Src",Src);
+formData.append("RentACarID",rentACarId);
+formData.append("CoverType",CoverType);
 
 
 
 
-    const response = await fetch(apiServer+"CreateProduct", {
+    const response = await fetch(apiServer+"CreateRentACarSRC", {
       method: "POST",
       headers: {
         'UserId': userInfo.UserId,         
@@ -201,58 +156,9 @@ formData.append("Description",Description);
 }
 
 
-const handleEditAdmin = async () => {
-
- 
-
-Show.showLoading("Processing Data");
-  try {
-
-const formData = new FormData()
-formData.append("AdminId",userInfo.UserId)
-formData.append("MenuId",MenuId);
-formData.append("CategoryId",CategoryId);
-formData.append("Picture",picture);
-formData.append("Title",Title);
-formData.append("Price",Price);
-formData.append("Quantity",Quantity);
-formData.append("Size",Size);
-formData.append("ProductId",ProductId);
-formData.append("Description",Description);
 
 
-    const response = await fetch(apiServer+"UpdateProduct", {
-      method: "POST",
-      headers: {
-        'UserId': userInfo.UserId,         
-        'SessionId': userInfo.SessionId    
-      },
-      body:formData
-    });
-
-    const data = await response.json();
- 
-
-    if (response.ok) {
-      
-      Show.hideLoading();
-
-      Show.Success(data.message);
-       
-      window.location.reload()
-      
-    } else {
-      Show.Attention(data.message);
-    }
-  } catch (error) {
-
-    Show.Attention("An error has occured");
-   
-  }
-
-}
-
-const handleDeleteAdmin = async (id) => {
+const handleDeleteAdmin = async (Id, ExploreID) => {
 
  
 
@@ -260,11 +166,12 @@ const handleDeleteAdmin = async (id) => {
     try {
   
   const formData = new FormData()
-  formData.append("UserId", id) 
+  formData.append("RentACarID", ExploreID) 
+  formData.append("Id", Id) 
   formData.append("AdminId",userInfo.UserId)
 
   
-      const response = await fetch(apiServer+"DeleteAdmin", {
+      const response = await fetch(apiServer+"DeletedRentACarSRC", {
         method: "POST",
         headers: {
           'UserId': userInfo.UserId,         
@@ -300,27 +207,14 @@ const handleDeleteAdmin = async (id) => {
 
 
 
-  const handleEdit = (id) => {
-    console.log('Edit entry ID:', id);
-  };
 
 
 
-  const handleActionBegin = (args) => {
-    if (args.requestType === 'save') {
-      const updatedData = args.data;
-      handleEdit(updatedData);
-    }
 
-    if (args.requestType === 'delete') {
-      const deletedData = args.data[0]; 
-      handleDeleteAdmin(deletedData.UserId);
-    }
-  };
-
+ 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
-    setPicture(file);
+    setSrc(file);
   
     // Preview the selected image
     const reader = new FileReader();
@@ -329,109 +223,197 @@ const handleDeleteAdmin = async (id) => {
     };
     reader.readAsDataURL(file);
   };
-  
+
+
 const fileType = [
     {id:1, name:"Image"},
     {id:2, name:"Video"},
 ]
 
 
+// Define the menu items array
+const menuItems = [
+
+  {
+    icon: <MdDelete />,
+    text: "Delete Details",
+    type: "function",
+    onClick: (id, RentACarID) => {
+      handleDeleteAdmin(id, RentACarID); // Assuming this function is defined in your component
+    },
+    columnNames: ['id','RentACarID'] // Specify the column name for the ID here
+  },
+
+
+
+
+];
+
+
+ const exploreGrid = [
+  { accessorKey: "id", header: "ID" },
+  { accessorKey: "RentACarID", header: "RentACar ID" },
+  { accessorKey: "Src", header: "Source" },
+  ];
+
+ const exploreMediaGrid = [
+  { accessorKey: "Src", header: "Source" },
+ ];
+
+
+
+
+const [activeStep, setActiveStep] = useState(0);
+const steps = ['Select Cover Type', 'Upload Media',  'Complete'];
+
+const getStepIcon = (step) => {
+  switch (step) {
+    case 0:
+      return <GiBookCover />;
+    case 1:
+      return <VideoLabelIcon />;
+    case 2:
+      return  <CheckIcon />;
+    case 3:
+      return <CheckIcon />;
+    default:
+      return <CheckIcon />;
+  }
+};
+
+
+
+const handleNext = () => {
+  setActiveStep((prevActiveStep) => prevActiveStep + 1);
+};
+
+const handleBack = () => {
+  setActiveStep((prevActiveStep) => prevActiveStep - 1);
+};
+
+const handleReset = () => {
+  setActiveStep(0);
+};
+
+
   return (
     <div>
-      <Header category="Website Configuration" title="Explore" />
+      <Header category="Website Configuration" title="Rent Car Sources" />
 
-      <div className="wwd-row">
 
-        <div className="card" style={{ backgroundColor: localStorage.getItem("themeMode") === "Light" ? "#26293C" : "white" }}>
-          <div className="sec-title" style={{ color: localStorage.getItem("colorMode"), padding: "2rem" }}>Add  Explore Slides</div>
 
-          <AdmitStudentRole>
 
-          <Selector placeholder="Select Type" dataList={fileType} dataKey="name" dataValue="name" setMethod={(method) => setCategoryId(method)} />
-            
-            {
-                CategoryId=="Image"?<>
+<div className="wwd-row">
+      <div className="card" style={{ backgroundColor: localStorage.getItem("themeMode") === "Light" ? "#26293C" : "white", padding: "2rem" }}>
+        
+       <Stepper activeStep={activeStep} alternativeLabel sx={{ padding: '2rem 0' }}>
+          {steps.map((label, index) => (
+            <Step key={label}>
+              <StepLabel sx={{ color: localStorage.getItem("themeMode") === "Light" ? "orange" : "blue" }} StepIconComponent={() => getStepIcon(index)} >
+               <span style={{ color: localStorage.getItem("themeMode") === "Light" ? "orange" : "blue" }}>{label}</span> 
                 
-                
-                {previewImage && (
-              <div style={{ marginTop: "1rem" }}>
-                <img src={previewImage} alt="Preview" style={{ width: "auto", height: "40vh" }} />
-              </div>
-            )}
-      
+                </StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+        
+        <Box sx={{ padding: 3 }}>
+          {activeStep === 0 && (
             <div>
-              <FormLable style={{ color: localStorage.getItem("colorMode") }}> Select Image</FormLable>
-              <FormInputStudent
-               type="file"
-               required
-               placeholder=""
-               accept=".jpg, .png, .jpeg, .ico, .webp"
-               onChange={handleImageChange}
-               
+              <div className="sec-title" style={{ color: localStorage.getItem("colorMode"), padding: "2rem" }}>Add Media</div>
+              <Selector
+                placeholder="Select CoverType"
+                dataList={fileType}
+                dataKey="name"
+                dataValue="name"
+                setMethod={(method) => setCoverType(method)}
               />
             </div>
+          )}
 
-                
-                
-                </>:<></>
-            }
+          {activeStep === 1 && (
+            <div>
+              {CoverType === "Image" && (
+                <>
+                  {previewImage && (
+                    <div style={{ marginTop: "1rem" }}>
+                      <img src={previewImage} alt="Preview" style={{ width: "auto", height: "40vh" }} />
+                    </div>
+                  )}
+                  <FormLable style={{ color: localStorage.getItem("colorMode") }}>Cover</FormLable>
+                  <FormInputStudent
+                    type="file"
+                    required
+                    placeholder=""
+                    accept=".jpg, .png, .jpeg, .ico, .webp"
+                    onChange={handleImageChange}
+                  />
+                </>
+              )}
+
+              {CoverType === "Video" && (
+                <>
+                  {previewImage && (
+                    <video
+                      controls
+                      width="200"
+                      height="200"
+                      src={previewImage}
+                      style={{ width: "auto", height: "40vh" }}
+                    >
+                      Your browser does not support the video tag.
+                    </video>
+                  )}
+                  <FormLable style={{ color: localStorage.getItem("colorMode") }}>Cover</FormLable>
+                  <FormInputStudent
+                    type="file"
+                    required
+                    placeholder=""
+                    accept=".mp4, .mov"
+                    onChange={handleImageChange}
+                  />
+                </>
+              )}
+            </div>
+          )}
+
+        {activeStep === 2 && (
+            <div>
+              <Typography>
+              <span style={{ color: localStorage.getItem("themeMode") === "Light" ? "orange" : "blue" }}> All steps completed. Ready to submit.</span> 
+               </Typography>
+              <AdmitButton3
+                background={localStorage.getItem("colorMode")}
+                color="white"
+                border={localStorage.getItem("colorMode")}
+                onClick={handleCreateAdmin}
+              >
+                Submit
+              </AdmitButton3>
+            </div>
+          )}
 
 
-            {
-                CategoryId=="Video"?<>
-                
-                
-                {previewImage && (
-              <video
-              controls
-              width="200"
-              height="200"
-              src={previewImage}
-              style={{ width: "auto", height: "40vh" }}
+          <div style={{ display: "flex", justifyContent: "space-between", marginTop: "2rem" }}>
+            <Button
+              disabled={activeStep === 0}
+              onClick={handleBack}
+              sx={{ mt: 2, mr: 1 }}
             >
-              Your browser does not support the video tag.
-            </video>
-            )}
-      
-            <div>
-              <FormLable style={{ color: localStorage.getItem("colorMode") }}> Select Video</FormLable>
-              <FormInputStudent
-               type="file"
-               required
-               placeholder=""
-               accept=".mp4, .mov"
-               onChange={handleImageChange}
-               
-              />
-            </div>
-
-                
-                
-                </>:<></>
-            }
-
-           
-         
-          </AdmitStudentRole>
-
-          <AdmitButton3
-            background={localStorage.getItem("colorMode")}
-            color="white"
-            border={localStorage.getItem("colorMode")}
-            style={{ marginBottom: "1rem" }}
-            onClick={()=>{ handleCreateAdmin()}}       
-            >Add
-          </AdmitButton3>
-
-         
-
-
-
-        </div>
-
-     
-
+              Back
+            </Button>
+            <Button
+              variant="contained"
+              onClick={activeStep === steps.length - 1 ? handleReset : handleNext}
+              sx={{ mt: 2 }}
+            >
+              {activeStep === steps.length - 1 ? 'Reset' : 'Next'}
+            </Button>
+          </div>
+        </Box>
       </div>
+    </div>
+
 
       <div style={{ marginTop: "2rem", padding: "1rem" }}>
         <span>
@@ -442,35 +424,24 @@ const fileType = [
               fontSize: "1.5rem",
             }}
           >
-          Explore List
+          Rent Car Sources List
           </u>
         </span>
 
-        <GridComponent
-           id="gridcomp"
-      toolbar={['Search']}  // Add the search bar
- 
-          dataSource={AdminList}
-          enableHover={true}
-          allowPaging
-          allowSorting
-          allowExcelExport
-          allowPdfExport
-          contextMenuItems={contextMenuItems}
-          actionBegin={handleActionBegin}
-          style={{ backgroundColor: localStorage.getItem("colorMode") }}
-        >
-          <ColumnsDirective>
-            {productImageGrid.map((item, index) => (
-              <ColumnDirective key={index} {...item} />
-            ))}
-          </ColumnsDirective>
-          <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport, Search, Toolbar]} />
+        <HydotTable 
+  columns={exploreGrid} 
+  data={Explore} 
+  media={exploreMediaGrid} 
+  colorMode={localStorage.getItem("colorMode")}
+  menuItems={menuItems}
 
-        </GridComponent>
+/>;
+
+       
       </div>
     </div>
   );
 }
 
-export default RentACarSlidesDetails;
+export default Explore;
+
