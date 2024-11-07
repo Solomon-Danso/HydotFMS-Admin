9ea5454499d2945e78c88261859b162987c0fee0
Solomon <solomondanso2023@gmail.com>
2024-11-07 19:20:21 +0000

    update

--- src/data/Endpoint.js
+++ src/data/Endpoint.js
@@ -3,8 +3,8 @@ const isLocalhost = window.location.hostname === "localhost";
 // Set API endpoints based on environment
 export const apiServer = isLocalhost 
   ? "http://127.0.0.1:8000/api/" 
-  : "https://api.commerce.hydottech.com/api/";
+  : "https://motorsapi.hydottech.com/api/";
 
 export const apiMedia = isLocalhost 
   ? "http://127.0.0.1:8000/storage/" 
-  : "https://api.commerce.hydottech.com/storage/";
+  : "https://motorsapi.hydottech.com/storage/";
